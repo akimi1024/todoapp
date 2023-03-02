11bb8c74dba66f7ab1f6d14966ffa441eb6b7ae1
宮澤昭海 <>
2023-03-03 00:05:45 +0900

    boardscontrollerの作成

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,20 @@
+class BoardsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def updated
+  end
+
+  def destroy
+  end
+end
